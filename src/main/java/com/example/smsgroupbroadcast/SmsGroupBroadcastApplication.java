f1d247a537d7ec277ff0cc3c3961b0a8d540ae9c
Matthew Gilliard <matthew@gilliard.lol>
2021-08-10 12:45:14 +0100

    Initialized project from start.spring.io

---
+++ src/main/java/com/example/smsgroupbroadcast/SmsGroupBroadcastApplication.java
@@ -0,0 +1,13 @@
+package com.example.smsgroupbroadcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsGroupBroadcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsGroupBroadcastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smsgroupbroadcast/SmsGroupBroadcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smsgroupbroadcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsGroupBroadcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
